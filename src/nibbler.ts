a3224331a1669b74487adcd1278886387fb4c090
Andreas Nordseth <anordseth@gmail.com>
2024-02-21 22:05:06 +0100

    typo

--- src/nibbler.ts
+++ src/nibbler.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import * as tc from '@actions/tool-cache';
 
 export const getNibblerTool = async (): Promise<string> => {
-    const version = core.getInput('version');
+    const version = core.getInput('nibbler-version');
     let nibblerPath = tc.find('nibbler', version);
     if (!nibblerPath) {
         const nibblerTar = await tc.downloadTool(`https://github.com/nordseth/Nibbler/releases/download/v${version}/Nibbler.${version}_linux-x64.tar.gz`);
