85ac2eb650e6bdafc18931f51efaee2f607443f7
Andreas Nordseth <anordseth@gmail.com>
2024-02-21 21:02:56 +0100

    add typescript

--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
-const core = require('@actions/core');
-const tc = require('@actions/tool-cache');
+import core from '@actions/core';
+import tc from '@actions/tool-cache';
 
-try {
+async function getNibblerTool(): Promise<string> {
   const version = core.getInput('version');
-  nibblerPath = tc.find('nibbler', version);
+  let nibblerPath = tc.find('nibbler', version);
   if (!nibblerPath) {
     const nibblerTar = await tc.downloadTool(`https://github.com/nordseth/Nibbler/releases/download/v${version}/Nibbler.${version}_linux-x64.tar.gz`);
     const nibblerTmp = await tc.extractTar(nibblerTar);
     nibblerPath = await tc.cacheFile(`${nibblerTmp}/nibbler`, 'nibbler', 'nibbler', version);
   }
 
+  return `${nibblerPath}/nibbler`;
+}
+
+
+try {
+  const nibbler = await getNibblerTool();
+  
 } catch (error) {
   core.setFailed(error.message);
 }
