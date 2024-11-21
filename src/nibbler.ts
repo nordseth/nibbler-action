960b725a984a024f65743db2c87f2a88a443f0eb
Andreas Nordseth <anordseth@gmail.com>
2024-11-21 20:44:48 +0100

    Nibbler 1.12.0 and multi arch support (#4)

--- src/nibbler.ts
+++ src/nibbler.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import * as tc from '@actions/tool-cache';
+import { familySync, MUSL } from 'detect-libc';
 
 export const getNibblerTool = async (): Promise<string> => {
     const version = core.getInput('nibbler-version');
@@ -8,7 +9,8 @@ export const getNibblerTool = async (): Promise<string> => {
     let nibblerPath = tc.find('nibbler', version);
 
     if (!nibblerPath) {
-        const downloadUrl = `https://github.com/nordseth/Nibbler/releases/download/v${version}/Nibbler.${version}_linux-x64.tar.gz`
+        const arch = GetArch();
+        const downloadUrl = `https://github.com/nordseth/Nibbler/releases/download/v${version}/Nibbler.${version}_${arch}.tar.gz`
         core.info(`downloading from ${downloadUrl}`)
         const nibblerTar = await tc.downloadTool(downloadUrl);
         const nibblerExtracted = await tc.extractTar(nibblerTar);
@@ -17,4 +19,14 @@ export const getNibblerTool = async (): Promise<string> => {
 
     core.debug(`nibbler-path: ${nibblerPath}`)
     return `${nibblerPath}/nibbler`;
+}
+
+function GetArch() {
+    if (process.platform != 'linux') {
+        return "win-x64";
+    } else {
+        const musl = familySync() == MUSL ? "musl-" : "";
+        const arch = process.arch == 'arm64' ? "arm64" : "x64";
+        return `linux-${musl}${arch}`;
+    }
 }
